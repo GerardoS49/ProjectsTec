1670ae1c4dd69795c1c39605c2509a2a8eafb10c
GerardoS49 <AL02988382@tecmilenio.mx>
2024-12-03 21:21:43 -0600

    Create Clase.java

---
+++ Clase.java
@@ -0,0 +1,25 @@
+import java.io.Serializable;
+
+public class Patient implements Serializable {
+    private String name;
+    private int age;
+    private String medicalHistory;
+
+    public Patient(String name, int age, String medicalHistory) {
+        this.name = name;
+        this.age = age;
+        this.medicalHistory = medicalHistory;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getMedicalHistory() {
+        return medicalHistory;
+    }
+}
